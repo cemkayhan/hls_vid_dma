93baf3729dd3d9e77596bf4ceda52ff8aa54eaa7
Cem Kayhan <cemkayhan@hotmail.com>
2024-04-17 15:18:15 +0300

    bug fix

--- macros/export.tcl
+++ macros/export.tcl
@@ -6,6 +6,7 @@ source add_files.tcl
 
 source config_rtl.tcl
 source config_dataflow.tcl
+source config_interface.tcl
 csynth_design
 file copy -force -- vitis_hls.log $prj_dir/solution1/vitis_hls.log
 export_design -flow $flow -format $format
